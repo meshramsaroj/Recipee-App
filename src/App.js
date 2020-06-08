091079c8e51478004321f3f669e971fed42303bd
meshramsaroj <meshramsaroj>
2020-06-08 18:01:41 +0530

    Did css changes

--- src/App.js
+++ src/App.js
@@ -1,63 +1,66 @@
-import './App.css'
-import './createReciepeApp/recipe-module.css'
+import "./App.css";
+import "./createReciepeApp/recipe-module.css";
 
-import React, { useEffect, useState } from 'react'
-import Recipe from './createReciepeApp/Recipe';
-
+import React, { useEffect, useState } from "react";
+import Recipe from "./createReciepeApp/Recipe";
 
 const App = () => {
-  const APP_ID = '75ca12a4';
-  const APP_KEY = '151e5a493e29dd383e34e881da1643ef';
+  const APP_ID = "75ca12a4";
+  const APP_KEY = "151e5a493e29dd383e34e881da1643ef";
 
-  const [recipes, setRecipe] = useState([])
-  const [search, setSearch] = useState("")
-  const [query, setQuery] = useState('banana');
+  const [recipes, setRecipe] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("banana");
   useEffect(() => {
-    //console.log('effect has been run')
     getReciepes();
-    console.log(recipes)
-    console.log('let say about mydata')
-
   }, [query]);
 
   const getReciepes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
-    console.log(data);
-    setRecipe(data.hits)
-
-  }
+    setRecipe(data.hits);
+  };
   const getSearch = e => {
     e.preventDefault();
-    setQuery(search)
-    setSearch('');
-  }
+    setQuery(search);
+    setSearch("");
+  };
 
   const updateSeacrh = e => {
-    setSearch(e.target.value)
-    //console.log(search)
-
-  }
+    setSearch(e.target.value);
+  };
   return (
     <div className="App">
       <div>
         <form onSubmit={getSearch} className="search-form">
-          <input className="search-bar" type=" text" value={search} onChange={updateSeacrh} />
-          <button className="search-btn" type="submit">search</button>
+          <input
+            placeholder="Enter Recipee Name"
+            className="search-bar"
+            type=" text"
+            value={search}
+            onChange={updateSeacrh}
+          />
+          <button className="search-btn" type="submit">
+            search
+          </button>
         </form>
       </div>
 
       <div className="recipes">
-        {recipes.map(recipeList =>
-          <Recipe key={recipeList.recipe.label} title={recipeList.recipe.label} calories={recipeList.recipe.calories}
-            image={recipeList.recipe.image} ingredient={recipeList.recipe.ingredients} />
-        )}
+        {recipes.map(recipeList => (
+          <Recipe
+            key={recipeList.recipe.label}
+            title={recipeList.recipe.label}
+            calories={recipeList.recipe.calories}
+            image={recipeList.recipe.image}
+            ingredient={recipeList.recipe.ingredients}
+          />
+        ))}
       </div>
-
-
     </div>
   );
-}
+};
 
-
-export default App
+export default App;
--- src/createReciepeApp/RecipeIngredient.js
+++ src/createReciepeApp/RecipeIngredient.js
@@ -1,20 +1,18 @@
-import React, { } from 'react'
+import React from "react";
 
 const RecipeIngredient = ({ ingredient }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ul>
+        {ingredient.map(ingredient => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
+      <p>{calories}</p>
+      <img src={image} />
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h1>{title}</h1>
-            <ul>
-                {ingredient.map(ingredient => (
-                    <li>{ingredient.text}</li>
-                ))}
-            </ul>
-            <p>{calories}</p>
-            <img src={image} />
-        </div>
-    )
-}
-
-
-export default RecipeIngredient
+export default RecipeIngredient;
